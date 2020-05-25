ee6eacab26dd8c1b668dc11c46be4e619e64e8ff
Mehul Gohil <mehul.gohil0810@gmail.com>
2020-05-25 16:48:24 +0530

    fix: improve code for js for activate and deactivate license to work properly

--- assets/src/js/admin/main.js
+++ assets/src/js/admin/main.js
@@ -2,7 +2,6 @@ document.addEventListener( 'DOMContentLoaded', () => {
 	const licenseKeyTextFields = Array.from( document.querySelectorAll( 'input[name="licenseKey"]' ) );
 	const activateLicenseBtns = Array.from( document.querySelectorAll( '.mg-activate-license-btn' ) );
 	const deactivateLicenseBtns = Array.from( document.querySelectorAll( '.mg-deactivate-license-btn' ) );
-	const spinner = document.querySelector( '.spinner' );
 
 	/**
 	 * This section of JS will be used to ensure that the "Activate License" button
@@ -29,28 +28,27 @@ document.addEventListener( 'DOMContentLoaded', () => {
 		activateLicenseBtn.addEventListener( 'click', ( e ) => {
 			e.preventDefault();
 
-			// spinner.style.visibility = 'visible';
-
 			const parentElement = e.target.parentNode;
-			const licenseKey = e.target.parentNode.querySelector( 'input[name="licenseKey"]' );
+			const licenseKey = parentElement.querySelector( 'input[name="licenseKey"]' );
 			const xhttp = new XMLHttpRequest();
 			const data = new FormData();
 
-			data.append( 'action', 'mg_licenses_activate_product' );
+			data.append( 'action', 'mg_license_manager_activate_product' );
 			data.append( 'slug', licenseKey.getAttribute( 'data-slug' ) );
 			data.append( 'name', licenseKey.getAttribute( 'data-name' ) );
 			data.append( 'licenseKey', licenseKey.value );
 
 			xhttp.onload = function() {
 				const response = JSON.parse( xhttp.response );
-console.log(response);
+
 				if ( xhttp.status === 200 && response.success ) {
+					licenseKey.setAttribute( 'readonly', true );
+					e.target.classList.add( 'mg-hidden' );
+					e.target.nextElementSibling.classList.remove( 'mg-hidden' );
 
-				} else {
-
+					parentElement.querySelector( '.mg-license-not-active' ).classList.add( 'mg-hidden' );
+					parentElement.querySelector( '.mg-license-active' ).classList.remove( 'mg-hidden' );
 				}
-
-				spinner.style.visibility = 'hidden';
 			};
 			xhttp.open('POST', ajaxurl, true );
 			xhttp.send( data );
@@ -61,28 +59,27 @@ console.log(response);
 		deactivateLicenseBtn.addEventListener('click', (e) => {
 			e.preventDefault();
 
-			spinner.style.visibility = 'visible';
-
 			const xhttp = new XMLHttpRequest();
 			const parentElement = e.target.parentNode;
+			const licenseKey = parentElement.querySelector( 'input[name="licenseKey"]' );
 			const data = new FormData();
 
-			data.append('action', 'mg_ipay88_deactivate_license');
+			data.append( 'action', 'mg_license_manager_deactivate_product' );
+			data.append( 'slug', licenseKey.getAttribute( 'data-slug' ) );
+			data.append( 'name', licenseKey.getAttribute( 'data-name' ) );
+			data.append( 'licenseKey', licenseKey.value );
 
 			xhttp.onload = function () {
 				const response = JSON.parse(xhttp.response);
 
 				if (xhttp.status === 200 && response.success) {
-					parentElement.classList.add('give-hidden');
-					parentElement.previousElementSibling.classList.remove('give-hidden');
-					document.querySelector('.mggi88-license-connected').classList.add('give-hidden');
-					document.querySelector('.mggi88-license-disconnected').classList.remove('give-hidden');
-					licenseErrors.innerHTML = `<p class="notice notice-success">${response.data.message}</p>`;
-				} else {
-					licenseErrors.innerHTML = `<p class="notice notice-error">${response.data.message}</p>`;
-				}
+					licenseKey.removeAttribute( 'readonly' );
+					e.target.classList.add( 'mg-hidden' );
+					e.target.previousElementSibling.classList.remove( 'mg-hidden' );
 
-				spinner.style.visibility = 'hidden';
+					parentElement.querySelector( '.mg-license-not-active' ).classList.remove( 'mg-hidden' );
+					parentElement.querySelector( '.mg-license-active' ).classList.add( 'mg-hidden' );
+				}
 			};
 			xhttp.open('POST', ajaxurl, true);
 			xhttp.send(data);
